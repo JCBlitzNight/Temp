94e3dd2df608622c45b7ce2424b2de991edafc9b
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-02-02 13:22:09 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -24,3 +24,22 @@ brute_forcers = [ip for ip, count in ip_count.items() if count >= threshold]
 print("Brute Forcers:")
 for ip in brute_forcers:
     print(ip)
+
+    
+    TCP Connect Scan: Detects a full three-way handshake, with the SYN, SYN-ACK, and ACK packets.
+
+TCP SYN Scan: Detects SYN packets sent to a target without completing the three-way handshake.
+
+TCP FIN Scan: Detects FIN packets sent to a target to determine which ports are open or closed.
+
+TCP Null Scan: Detects packets with all flags set to zero, which can be used to identify open ports.
+
+TCP Xmas Scan: Detects packets with the FIN, PSH, and URG flags set, which can be used to identify open ports.
+
+UDP Scan: Detects packets sent to UDP ports, which can be used to identify open or closed ports.
+
+ACK Scan: Detects ACK packets sent to a target to determine which ports are open or closed.
+
+Window Scan: Detects the size of the TCP window on a target, which can be used to identify open or closed ports.
+
+Maimon Scan: Detects packets sent with the ACK and URG flags set, which can be used to identify open ports.
