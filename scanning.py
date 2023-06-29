995834a2f03a8c6eb73ecbc456dcbdcf771b53aa
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-06-29 11:52:51 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -7,14 +7,15 @@ csv_file = 'path/to/csv/file.csv'
 # Path to the text file containing IP addresses
 ip_file = 'path/to/ip/file.txt'
 
-# Read IP ranges and countries from the CSV file
+# Read IP ranges, ISPs, and countries from the CSV file
 ip_ranges = {}
 with open(csv_file, 'r') as file:
     csv_reader = csv.reader(file)
     for row in csv_reader:
-        ip_range = row[0]
-        country = row[1]
-        ip_ranges[ip_range] = country
+        country = row[0]
+        ip_range = row[1]
+        isp = row[2]
+        ip_ranges[ip_range] = isp
 
 # Function to check if an IP address falls within a subnet
 def ip_in_subnet(ip, subnet):
