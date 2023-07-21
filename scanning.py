4839fc16cd2d21ffa27efd20a05919e0f673e380
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-07-21 11:46:26 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,10 +1,10 @@
-// Set colors based on continent information
-bubbleSeries.events.on("beforevalidated", function (event) {
-  bubbleSeries.dataItems.each(function (dataItem) {
-    var continent = dataItem.get("continent");
-    if (!colorsByContinent[continent]) {
-      colorsByContinent[continent] = colors.getIndex(); // Assign new color to the continent
-    }
-    dataItem.get("circleTemplate").set("fill", colorsByContinent[continent]);
-  });
-});
+ try:
+        # Try to find the ISO code using pycountry_convert
+        country_code = pycountry_convert.country_name_to_country_alpha2(country_name, cn_name_format="default")
+        continent_code = pycountry_convert.country_alpha2_to_continent_code(country_code)
+
+        if country_code and continent_code:
+            country_data.append({"id": country_code, "category": country_name, "value": count, "continent": continent_code})
+
+    except Exception as e:
+        print(f"Error processing country '{country_name}': {e}")
