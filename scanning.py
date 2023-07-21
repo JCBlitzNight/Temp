65e9a6a7a9ef0bc780fa58428907514ad1bc1a6f
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-07-21 11:56:19 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,10 +1,13 @@
- try:
-        # Try to find the ISO code using pycountry_convert
+def get_country_code(country_name):
+    try:
+        # Try the default format first
         country_code = pycountry_convert.country_name_to_country_alpha2(country_name, cn_name_format="default")
-        continent_code = pycountry_convert.country_alpha2_to_continent_code(country_code)
-
-        if country_code and continent_code:
-            country_data.append({"id": country_code, "category": country_name, "value": count, "continent": continent_code})
-
-    except Exception as e:
-        print(f"Error processing country '{country_name}': {e}")
+        return country_code
+    except Exception:
+        try:
+            # If default format fails, try the fuzzy format
+            country_code = pycountry_convert.country_name_to_country_alpha2(country_name, cn_name_format="fuzzy")
+            return country_code
+        except Exception:
+            # If both formats fail, try the custom mappings
+            return custom_country_mappings.get(country_name, None)
