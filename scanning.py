31b4cc27924af436e4ca8daf5bc9483e43d5c182
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-07-24 13:44:41 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,13 +1,27 @@
-def get_country_code(country_name):
-    try:
-        # Try the default format first
-        country_code = pycountry_convert.country_name_to_country_alpha2(country_name, cn_name_format="default")
-        return country_code
-    except Exception:
-        try:
-            # If default format fails, try the fuzzy format
-            country_code = pycountry_convert.country_name_to_country_alpha2(country_name, cn_name_format="fuzzy")
-            return country_code
-        except Exception:
-            # If both formats fail, try the custom mappings
-            return custom_country_mappings.get(country_name, None)
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>AMCharts Heatmap & XYChart Example</title>
+  <!-- Tailwind CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+  <!-- AMCharts Core and Charts libraries -->
+  <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+  <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+  <!-- AMCharts Themes -->
+  <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
+</head>
+<body class="bg-gray-100 min-h-screen flex justify-center items-center">
+  <div class="container mx-auto p-4">
+    <h1 class="text-4xl font-bold mb-4">Chart Examples</h1>
+    <!-- Heatmap Chart -->
+    <div class="bg-white rounded-lg shadow-md p-4 mb-4">
+      <h2 class="text-2xl font-bold mb-4">Heatmap Chart</h2>
+      <div id="heatmapChart" class="w-full h-64"></div>
+    </div>
+
+    <!-- XYChart -->
+    <div class="bg-white rounded-lg shadow-md p-4">
+      <h2 class="text-2xl font-bold mb-4">XYChart</h2>
+      <div id="xyChart" class="w-full h-64"></div>
+    </div>
+  </div>
