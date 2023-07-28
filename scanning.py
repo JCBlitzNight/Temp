79d369ad7b5dd2b431a1ea470452aa75aaf5e5f7
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-07-28 10:21:13 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,38 +1,44 @@
 <!DOCTYPE html>
 <html>
 <head>
-<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-
-<!-- Tailwind CSS -->
-<link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet"> 
-
-<style>
-#chartdiv {
-  width: 100%;
-  height: 500px;
-}
-</style>
+  <script src="https://cdn.jsdelivr.net/npm/fuse.js"></script>
+  
+  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
 
 </head>
 
 <body>
 
-<div id="chartdiv"></div>
+<div class="container mx-auto px-4 max-w-4xl py-8">
 
-<!-- Modal -->
-<div id="modal" class="fixed hidden inset-0 bg-gray-900 bg-opacity-50 overflow-y-auto">
+  <h1 class="text-4xl mb-6">IP Address Table</h1>
 
-  <div class="relative top-40 mx-auto p-5 border w-96 max-h-600px shadow-lg rounded-md bg-white">
+  <div class="bg-white shadow rounded-lg overflow-x-auto">
 
-    <h3 class="text-lg font-medium mb-2">IP Addresses</h3>
+    <div class="px-4 py-5 sm:px-6 flex justify-between items-center">
+      <h3 class="text-lg leading-6 font-medium text-gray-900">
+        IP Address Information  
+      </h3>
+      <input class="search-input py-2 px-3 border border-gray-300 rounded-md" placeholder="Search IP...">
+    </div>
 
-    <input id="search" type="text" class="border p-2 w-full mb-4" placeholder="Search IPs..."> 
+    <table class="min-w-full divide-y divide-gray-200">
+      <thead>
+        <tr>
+          <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase">IP</th>
+          <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase">Location</th>
+          <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase">ISP</th>
+          <th class="px-6 py-3 bg-gray-50 text-left text-xs font-medium text-gray-500 uppercase">Tags</th>
+        </tr>
+      </thead>
 
-    <div id="ips"></div>
+      <tbody id="table-rows" class="bg-white divide-y divide-gray-200">
+        <!-- Rows inserted here -->  
+      </tbody>
+    
+    </table>
 
-    <div id="pagination"></div>
+    <div id="pagination" class="px-4 py-3 text-xs font-medium"></div>
 
   </div>
 
@@ -40,78 +46,80 @@
 
 <script>
 
-// Chart code 
-
-// IPs dataset
-const ips = [
-  /* IP array */ 
+const ipData = [
+  {
+    ip: "192.168.1.1",  
+    location: "New York, NY",
+    isp: "Spectrum",
+    tags: ["home", "wireless"]
+  },
+  // more rows  
 ];
 
-// Modal handlers
-const modal = document.querySelector('#modal'); 
-const ipsDiv = document.querySelector('#ips');
+const resultsPerPage = 10;
+const totalPages = Math.ceil(ipData.length / resultsPerPage); 
 
-// Pagination
-const ipsPerPage = 10;
-let totalPages = Math.ceil(ips.length / ipsPerPage);
-let currentPage = 1;
+// Display paginated rows  
+function displayPage(pageNum) {
+  const startIndex = (pageNum - 1) * resultsPerPage;
+  const endIndex = startIndex + resultsPerPage;
+  const resultsToDisplay = ipData.slice(startIndex, endIndex);
 
-// Search
-function searchIPs(searchTerm) {
-  return ips.filter(ip => ip.includes(searchTerm));
-}
-
-// Display IPs
-function renderIPs(ipPages) {
-  let ipsHtml = "";
-  ipPages.forEach(ip => {
-    ipsHtml += `<p class="mb-2">${ip}</p>`;
+  let rowsHtml = '';
+  resultsToDisplay.forEach(item => {
+    rowsHtml += `
+      <tr>
+        <td class="px-6 py-4 whitespace-nowrap border-b">
+          ${item.ip} 
+        </td>
+        // etc...  
+      </tr>
+    `;
   });
-  
-  ipsDiv.innerHTML = ipsHtml;
-} 
 
-// Pagination 
-function handlePagination() {
-  let startIndex = (currentPage - 1) * ipsPerPage;
-  let endIndex = startIndex + ipsPerPage;
-  let currentIPs = ips.slice(startIndex, endIndex);
-  
-  renderIPs(currentIPs);
-  updatePagination();
+  document.querySelector('#table-rows').innerHTML = rowsHtml;
 }
 
-function updatePagination() {
-  let paginationHtml = "";
-  
-  for(let i=1; i<=totalPages; i++) {
-     paginationHtml += `<a href="#" class="page-btn px-2 py-1" data-page="${i}">${i}</a>`; 
+// Generate pagination
+function generatePagination(currentPage) {
+  let paginationHtml = '';
+  for (let i = 1; i <= totalPages; i++) {
+    const activeClass = i === currentPage ? 'bg-blue-500 text-white' : '';
+    paginationHtml += `<a class="page-link ${activeClass} cursor-pointer" data-page="${i}">${i}</a>`;
+  }  
+
+  document.querySelector('#pagination').innerHTML = paginationHtml; 
+}
+
+// Initially display first page  
+displayPage(1);
+generatePagination(1);
+
+// Handle search  
+const searchInput = document.querySelector('.search-input');
+
+searchInput.addEventListener('input', (e) => {
+
+  const searchTerm = e.target.value;
+
+  // Fuse.js filter
+  const fuse = new Fuse(ipData, options); 
+  const results = fuse.search(searchTerm);
+
+  // Display results
+  displayPage(1); 
+  generatePagination(1);
+
+  if (results.length > 0) {
+    displayPage(1, results); 
+  } else {
+    document.querySelector('#table-rows').innerHTML = `
+      <tr>
+        <td class="px-6 py-4 border-b" colspan="4">No results found</td>  
+      </tr>
+    `;
   }
   
-  document.querySelector('#pagination').innerHTML = paginationHtml;
-}
-
-// Search
-const searchBox = document.querySelector('#search');
-searchBox.addEventListener("input", (e) => {
-  const searchTerm = searchBox.value;
-  const filteredIPs = searchIPs(searchTerm);
-  
-  currentPage = 1;
-  
-  renderIPs(filteredIPs);
-  updatePagination();  
-});
-
-// Slice click handler
-series.slices.template.events.on("click", (event) => {
-  modal.classList.remove('hidden');
-  
-  // Show IPs
-  const ipData = event.target.dataItem.dataContext; 
-  renderIPs(ipData.ips);
-  
-  handlePagination();
 });
 
 </script>
