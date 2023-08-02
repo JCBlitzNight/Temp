f63f8d0989fd37b69b1752287fe7d37097356712
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-08-02 17:22:25 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,73 +1,30 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>IP Address Table</title>
-  
-  <!-- Tailwind CSS -->
-  <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"> 
-  
-  <!-- jQuery -->
-  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-</head>
+// Download button
+$('#download-btn').on('click', function() {
 
-<body class="p-4">
+  // Get selected slice
+  var selectedSlice = series.slices.getIndex(series.slices.getSelected());
 
-  <h1 class="text-2xl font-bold mb-4">IP Address Search</h1>
-  
-  <div>
-    <input id="ipInput" type="text" placeholder="Enter valid public IP" class="border p-2">
-    <button id="searchBtn" class="bg-blue-500 text-white px-4 py-2">Search</button>
-  </div>
-  
-  <table id="ipTable" class="hidden mt-8 table-auto border-collapse">
-    <thead>
-      <tr class="bg-gray-200">
-        <th class="border p-2">IP Address</th>
-        <th class="border p-2">Location</th>
-        <th class="border p-2">ISP</th>
-        <th class="border p-2">Tags</th>
-      </tr>
-    </thead>
-    <tbody>
-    </tbody>
-  </table>
+  // Get data
+  var ipData = selectedSlice.dataItem.dataContext;
+  var ips = ipData.ips;
 
-  <script>
-    // Validate IP on input
-    $('#ipInput').on('input', function() {
-      var ip = $(this).val(); 
-      if (!/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(ip)) {
-        $(this).addClass('border-red-500');
-      } else {
-        $(this).removeClass('border-red-500');
-      }
-    });
+  // Create text blob
+  var textBlob = new Blob([ips.join('\n')], {type: 'text/plain'});
 
-    // Search click handler
-    $('#searchBtn').click(function() {
-      var ip = $('#ipInput').val();
-      if (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.test(ip)) {
-        $.getJSON('db.json', function(data) {
-          var info = data[ip];
-          
-          // Clear table
-          $('#ipTable tbody').empty(); 
-          
-          // Add row to table
-          var row = '<tr>';
-          row += '<td class="border p-2">' + ip + '</td>';
-          row += '<td class="border p-2">' + info.location + '</td>';
-          row += '<td class="border p-2">' + info.isp + '</td>';
-          row += '<td class="border p-2">' + info.tags.join(', ') + '</td>';
-          row += '</tr>';
-          $('#ipTable tbody').append(row);
-          
-          // Show table
-          $('#ipTable').removeClass('hidden');
-        });
-      }
-    });
-  </script>
+  // Generate download link
+  var downloadLink = $('<a>').attr({
+    download: 'ips_' + ipData.tag + '.txt',
+    href: window.URL.createObjectURL(textBlob)
+  });
 
-</body>
-</html>
+  // Click download link
+  $('body').append(downloadLink);
+  downloadLink[0].click();
+  downloadLink.remove();
+
+});
+
+// Get selected slice
+function getSelectedSlice() {
+  return series.slices.getIndex(series.slices.getSelected()); 
+}
