125bafcf3d7385c4deba54628c2c7f9a3816be46
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-11-30 16:17:24 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,19 +1,23 @@
-import re
+import ast
+import textwrap
 
-class_pattern = r"class (\w+):"
-function_pattern = r"def (\w+)\("
+py_file = "mycode.py"
 
-with open('mycode.py') as f:
-    contents = f.read()
+classes = []  
+methods = []
 
-classes = re.findall(class_pattern, contents)
-functions = re.findall(function_pattern, contents)
-
+with open(py_file) as f:
+    tree = ast.parse(f.read())
+    
+    for node in ast.walk(tree):
+        if isinstance(node, ast.ClassDef):
+            classes.append(node.name)
+        elif isinstance(node, ast.FunctionDef): 
+            methods.append(f"{node.parent.name}.{node.name}")
+            
+print(f"// Parsed from {py_file}")                
 print("@startuml")
-for c in classes:
-    print(f"class {c} {{")
-    matched_functions = [f for f in functions if f.startswith(c+"_")] 
-    for f in matched_functions:
-        print(f"\t+ {f[len(c)+1:]}()") 
-    print("}\n")
+
+# Print class diagram like previous example 
+
 print("@enduml")
