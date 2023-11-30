3217625223e94c55b5a48535d3887a506bad1aeb
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-11-30 16:09:52 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,40 +1,19 @@
-import ast
 import re
 
+class_pattern = r"class (\w+):"
+function_pattern = r"def (\w+)\("
 
-class PlantUMLGenerator:
-    def __init__(self, python_file):
-        self.python_file = python_file  
-        self.class_names = []
-        self.functions = []
-    
-    def generate(self):
-        with open(self.python_file, 'r') as f:
-            self.tree = ast.parse(f.read())
-            self.visit_tree(self.tree)
-        
-        print("@startuml")
-                
-        for c in self.class_names:
-            print(f"class {c} {{")
-            
-            for f in self.functions:
-                if f.startswith(c):
-                    print(f"\t+ {f[len(c)+1:]}()")
-                    
-            print("}")
-        
-        print("@enduml") 
-        
-    def visit_tree(self, node):
-        if isinstance(node, ast.ClassDef):
-            self.class_names.append(node.name) 
-        
-        if isinstance(node, ast.FunctionDef):
-            self.functions.append(node.name)
- 
-        for child in ast.iter_child_nodes(node):
-            self.visit_tree(child)
-            
-generator = PlantUMLGenerator('mycode.py')
-generator.generate()
+with open('mycode.py') as f:
+    contents = f.read()
+
+classes = re.findall(class_pattern, contents)
+functions = re.findall(function_pattern, contents)
+
+print("@startuml")
+for c in classes:
+    print(f"class {c} {{")
+    matched_functions = [f for f in functions if f.startswith(c+"_")] 
+    for f in matched_functions:
+        print(f"\t+ {f[len(c)+1:]}()") 
+    print("}\n")
+print("@enduml")
