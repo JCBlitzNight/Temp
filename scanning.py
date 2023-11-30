9bacd56dcdebd058332d96cd6cf0d47cad7e6aff
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-11-30 15:52:38 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -4,45 +4,37 @@ import re
 
 class PlantUMLGenerator:
     def __init__(self, python_file):
-        self.python_file = python_file
+        self.python_file = python_file  
         self.class_names = []
-        self.associations = []
+        self.functions = []
     
     def generate(self):
         with open(self.python_file, 'r') as f:
             self.tree = ast.parse(f.read())
             self.visit_tree(self.tree)
         
-        plantuml_text ="@startuml\n"
-        
-        for m in self.associations:
-            src, dst = m
-            plantuml_text += f"{src} ..> {dst}\n"
-        
+        print("@startuml")
+                
         for c in self.class_names:
-            plantuml_text += f"class {c} {{\n"
+            print(f"class {c} {{")
             
-            # You can populate methods here e.g.
-            # plantuml_text += f"\t+ {method_name}()\n" 
-            
-            plantuml_text += "}\n"
-            
-        plantuml_text += "@enduml"
-        print(plantuml_text)
+            for f in self.functions:
+                if f.startswith(c):
+                    print(f"\t+ {f[len(c)+1:]}()")
+                    
+            print("}")
+        
+        print("@enduml") 
         
     def visit_tree(self, node):
         if isinstance(node, ast.ClassDef):
-            self.class_names.append(node.name)
+            self.class_names.append(node.name) 
         
         if isinstance(node, ast.FunctionDef):
-            # Can gather method names here
+            self.functions.append(node.name)
+ 
+        for child in ast.iter_child_nodes(node):
+            self.visit_tree(child)
             
-            pass # Remove this when gathering methods
-        
-        for child in ast.iter_child_nodes(node):  
-            if isinstance(child, ast.ClassDef):
-                self.visit_tree(child)
-
-if __name__ == "__main__":
-    gen = PlantUMLGenerator('my_code.py')
-    gen.generate()
+generator = PlantUMLGenerator('mycode.py')
+generator.generate()
