cca31c8f3e51e12bb036e045f1b8a9fb819cbbdd
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-11-30 15:48:16 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,28 +1,48 @@
-Phase 1:
+import ast
+import re
 
-    Completed onboarding and training on security tools and infrastructure
-    Learned the importance of an open and collaborative culture to enhance productivity
-    Developed foundational skills in security operations through job shadowing and hands-on experience
-    Gained soft skills through workshops on presentation delivery and conflict management
-    Presented learnings from a presentation workshop to my team
-    Challenged by adapting to the open culture, but improved by expanding my network
 
-Phase 2:
+class PlantUMLGenerator:
+    def __init__(self, python_file):
+        self.python_file = python_file
+        self.class_names = []
+        self.associations = []
+    
+    def generate(self):
+        with open(self.python_file, 'r') as f:
+            self.tree = ast.parse(f.read())
+            self.visit_tree(self.tree)
+        
+        plantuml_text ="@startuml\n"
+        
+        for m in self.associations:
+            src, dst = m
+            plantuml_text += f"{src} ..> {dst}\n"
+        
+        for c in self.class_names:
+            plantuml_text += f"class {c} {{\n"
+            
+            # You can populate methods here e.g.
+            # plantuml_text += f"\t+ {method_name}()\n" 
+            
+            plantuml_text += "}\n"
+            
+        plantuml_text += "@enduml"
+        print(plantuml_text)
+        
+    def visit_tree(self, node):
+        if isinstance(node, ast.ClassDef):
+            self.class_names.append(node.name)
+        
+        if isinstance(node, ast.FunctionDef):
+            # Can gather method names here
+            
+            pass # Remove this when gathering methods
+        
+        for child in ast.iter_child_nodes(node):  
+            if isinstance(child, ast.ClassDef):
+                self.visit_tree(child)
 
-    Expanded technical skills through trainings on cloud, threat hunting, digital forensics
-    Applied learnings in investigating real network, email, and file events
-    Developed expertise in analyzing office documents, PDFs, and web code for threats
-    Parsed email headers and sender info to uncover phishing attempts
-    Struggled with documentation initially but improved through supervisor feedback
-    Gained insights into department strategy through town halls
-    Networked with senior management through sharing panels
-
-Phase 3:
-
-    Successfully investigated incidents across various domains
-    Refined investigative approach through supervisor feedback
-    Accelerated technical abilities through hands-on work and training
-    Learned advanced techniques and real world scenarios through knowledge shares
-    Applied skills in Splunk BOTS competition with team members
-    Connected with leaders through interviews and mentorship
-    Volunteered to teach young women about cybersecurity at a STEM festival
+if __name__ == "__main__":
+    gen = PlantUMLGenerator('my_code.py')
+    gen.generate()
