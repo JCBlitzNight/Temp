000142f6dec98b3dc4db24c9c5e71bfb120495f2
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2023-12-01 10:59:56 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,37 +1,34 @@
 import socket
 from flask import Flask, request
+import os
+
 app = Flask(__name__)
 
 @app.route('/api/query')
 def api_query():
-    client_ip = request.args.get('ip') 
+    client_ip = request.args.get('ip')
     file_path = request.args.get('filepath')
     
-    if not valid_ip(client_ip):
+    if not valid_ip(client_ip): 
         return "Invalid IP", 400
     
     real_path = valid_file_path(file_path)
-    if not real_path:  
-        return "Invalid file path access", 400
+    if not real_path:
+        return "Invalid file path", 400 
 
-    # Additional logic with valid inputs   
-    ...
+    # API logic here 
 
-    return output
+    return "OK"
 
 def valid_ip(address):
     try: 
         socket.inet_aton(address)
-        return True
-
+        return True 
     except:
         return False
 
 def valid_file_path(path):
-    real_path = os.path.realpath(path)
+    real_path = os.path.realpath(path)  
     if real_path.startswith("/"):
-        return False 
-    return real_path 
-
-if __name__ != '__main__':
-     app.run(host='192.168.2.1')
+        return False     
+    return real_path  
