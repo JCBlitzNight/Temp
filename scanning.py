2d52b8c725a6f3b6a43dc63d7775d15998cfb607
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-01-31 20:04:12 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,2 +1,3 @@
-How proficient are you in using debuggers to analyze software and system behavior and identify potential tactics, techniques, and procedures?
-To what extent can you recognize tactics or techniques utilized by adversaries based on insights gained from debugger outputs?
+How proficient are you in analyzing various logs (e.g., system logs, network logs, application logs) to identify potential evidence of past intrusions?
+How well can you recognize and interpret intrusion indicators from logs, including but not limited to unusual patterns, anomalies, or suspicious activities?
+How effectively can you construct an incident timeline based on the analysis of logs, mapping the sequence of events related to a potential intrusion?
