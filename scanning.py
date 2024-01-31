1c68625c0bcef49a1f08ae9e69f2ab0157ef1d60
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-01-31 20:11:24 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,4 +1,2 @@
-How well can you assess the potential impact of a security incident on organizational assets and operations?
-To what extent are you proficient in conducting comprehensive risk assessments, considering vulnerabilities, threats, and potential consequences?
-How effective are you in proposing and implementing risk mitigation strategies based on assessment findings?
-How well can you communicate assessment findings, including potential impacts and risks, to relevant stakeholders?
+To what extent can you identify patterns and emerging trends from the data you analyze?
+How well can you apply trend analysis insights to inform decision-making processes?
