8828e2560292bc23e5f2ae30823ccdf5fd4f86b6
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-01-31 20:09:41 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,3 +1,4 @@
-How well can you process digital evidence collected from various source ?
-To what extent do you ensure that the processing of digital evidence aligns with legal standards and requirements, including making legally sound copies?
-How adept are you at implementing measures to protect digital evidence from alteration, loss, or unauthorized access during processing?
+How well can you assess the potential impact of a security incident on organizational assets and operations?
+To what extent are you proficient in conducting comprehensive risk assessments, considering vulnerabilities, threats, and potential consequences?
+How effective are you in proposing and implementing risk mitigation strategies based on assessment findings?
+How well can you communicate assessment findings, including potential impacts and risks, to relevant stakeholders?
