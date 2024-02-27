70a03e6bafd71a99a1f67de6a56e7176828ba668
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-02-27 21:29:05 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,2 +1,16 @@
-How well can you understand and interpret the structure of network packets?
-How proficient are you in detecting anomalies or irregularities within packet-level data ?
+Windows:
+1. How familiar are you with analyzing Windows event logs, including Security, Application, and System logs, to identify indicators of compromise (IOCs) or security-related anomalies? 
+2. How familiar are you with analyzing Windows artifacts, such as prefetch files, jump lists, link files, and registry hives, to reconstruct user activities and system usage patterns?
+3. How familiar are you with conducting file system analysis on Windows, including examining file timestamps, permissions, and file metadata to identify suspicious or unauthorized file activities?
+
+Mac:
+1. How proficient are you in capturing or identifying signs of malicious activity by parsing the Apple File System (APFS) manually, understanding its data structures, for forensic analysis on MacOS and iOS systems?
+2. How confident are you in capturing or identifying indicators of malicious activity through analysis of user activity logs and system configuration settings, including SSH and terminal history, firewall settings, keychain entries, and screen time usage on Mac and iOS devices?
+3. How confident are you in your ability to analyze disk layouts, partitions, encrypted volumes, disk images, CoreStorage configurations, APFS containers, and Bootcamp setups to effectively identify and respond to potential signs of malicious activity on MacOS systems?
+3. How confident are you in your ability to analyze data from a wide range of Apple applications such as Messages, FaceTime, Calendar, Contacts, and Photos, to reconstruct user activities and events?
+4. How confident are you in your ability to analyze application data on Apple devices, including browsing history, email communications, and various application interactions, to identify potential security incidents or user activities?
+
+Linux:
+1. How confident are you in identifying malicious activity on a Linux system, including analyzing system logs, user accounts, and hidden files?
+2. How confident are you in your ability to analyze suspicious files, reverse engineer Linux malware, and extract actionable intelligence from malware artifacts on a Linux system?
+3. How confident are you in performing analyzing malware artifacts in memory on a Linux system?
