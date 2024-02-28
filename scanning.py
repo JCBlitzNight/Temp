a1c9660e78285427e3f0807e8c1cab07c6197be7
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-02-28 23:58:43 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,13 +1,14 @@
 Windows:
-1. How familiar are you with analyzing Windows event logs, including Security, Application, and System logs, to uncover potential signs of unauthorised access, command and control, or lateral movement?
+1. How familiar are you with analyzing Windows event logs, including Security, Application, and System logs, to uncover potential signs of unauthorized access, command and control, or lateral movement?
 2. How familiar are you with analyzing Windows artifacts, such as prefetch files, jump lists, link files, and registry hives, to uncover potential signs of persistence or defense evasion?
 3. How familiar are you with conducting file system analysis on Windows, including examining file timestamps, permissions, and file metadata to identify unauthorized file modifications or execution?
 
-1. How proficient are you in capturing or identifying signs of malicious activity by parsing the Apple File System (APFS) manually, understanding its data structures, for forensic analysis on MacOS and iOS systems?
-2. How confident are you in capturing or identifying indicators of malicious activity through analysis of user activity logs and system configuration settings, including SSH and terminal history, firewall settings, keychain entries, and screen time usage on Mac and iOS devices?
-3. How confident are you in your ability to analyze application data on Apple devices, including browsing history, email communications, and various application interactions, to identify potential security incidents or user activities?
+Mac:
+1. How familiar are you with analyzing MacOS system logs, such as the system.log, appfirewall.log and secure.log, for signs of unauthorized access, command and control, or lateral movement?
+2. How familiar are you with analyzing Mac OS artifacts like quarantine files, launch agents, and plist files to uncover potential signs of persistence or defense evasion?
+3. How familiar are you with conducting file system analysis on Mac OS, including examining extended attributes, file timestamps, and permissions to identify unauthorized file modifications or execution?
 
 Linux:
-1. How confident are you in your ability to detect malicious activity on a Linux system by examining system logs, including login history, authentication-related files, and system logs, for suspicious activities or unauthorized access?
-2. How confident are you in your ability to identify potential security threats on a Linux system by analyzing user activity artifacts, such as user history files, configuration files, and application logs, to detect anomalous behavior or unauthorized actions?
-3. How confident are you in your ability to detect malicious activity on a Linux system by analyzing filesystem artifacts, such as hidden files and directories, newly created files, or files with suspicious permissions, using standard Linux command-line tools?
+1. How familiar are you with analyzing Linux logs found in /var/log, like auth.log and syslog, for signs of unauthorized access, command and control, or lateral movement?
+2. How familiar are you with analyzing Linux artifacts like cron jobs, bash history, and systemd services to uncover potential signs of persistence or defense evasion?
+3. How familiar are you with conducting file system analysis on Linux, including examining inode data, file timestamps, and permissions to identify unauthorized file modifications or execution?
