7fffaf7bf02c033410a80b6601bf543a49303040
Yip Jia Cheng <90367927+JCBlitzNight@users.noreply.github.com>
2024-05-17 15:48:56 +0800

    Update scanning.py

--- scanning.py
+++ scanning.py
@@ -1,14 +1,11 @@
-Windows:
-1. How familiar are you with analyzing Windows event logs, including Security, Application, and System logs, to uncover potential signs of unauthorized access, command and control, or lateral movement?
-2. How familiar are you with analyzing Windows artifacts, such as prefetch files, jump lists, link files, and registry hives, to uncover potential signs of persistence or defense evasion?
-3. How familiar are you with conducting file system analysis on Windows, including examining file timestamps, permissions, and file metadata to identify unauthorized file modifications or execution?
-
-Mac:
-1. How familiar are you with analyzing MacOS system logs, such as the system.log, appfirewall.log and secure.log, for signs of unauthorized access, command and control, or lateral movement?
-2. How familiar are you with analyzing Mac OS artifacts like quarantine files, launch agents, and plist files to uncover potential signs of persistence or defense evasion?
-3. How familiar are you with conducting file system analysis on Mac OS, including examining extended attributes, file timestamps, and permissions to identify unauthorized file modifications or execution?
-
-Linux:
-1. How familiar are you with analyzing Linux logs found in /var/log, like auth.log and syslog, for signs of unauthorized access, command and control, or lateral movement?
-2. How familiar are you with analyzing Linux artifacts like cron jobs, bash history, and systemd services to uncover potential signs of persistence or defense evasion?
-3. How familiar are you with conducting file system analysis on Linux, including examining inode data, file timestamps, and permissions to identify unauthorized file modifications or execution?
+Remove null bytes
+Find/Replace (\x00) -> (\u0000)
+Find/Replace (\\x00) -> ()
+Find/Replace (\\u0000) -> ()
+From Base64
+URL Decode
+Find/Replace (\\) -> ()
+JavaScript Beautify
+Unescape Unicode Characters
+Find/Replace (-encodedCommand ([A-Za-z0-9+/=]+)) -> ($1)
+From Base64
